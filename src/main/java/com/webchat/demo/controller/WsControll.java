2bb4fe8bb23f86f1489b8e82e993720d8607c902
陈龙飚 <714147655@qq.com>
2018-01-03 18:08:29 +0800

    111

--- src/main/java/com/webchat/demo/controller/WsControll.java
+++ src/main/java/com/webchat/demo/controller/WsControll.java
@@ -3,7 +3,7 @@ package com.webchat.demo.controller;
 import com.webchat.demo.domain.RequestMessage;
 import com.webchat.demo.domain.ResponseMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -12,7 +12,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public class WsControll {
 
     @MessageMapping("/welcome")
-    @SendTo("/topic/getResponse")
+    public void say1(RequestMessage message) {
+        this.say(message);
+//        return new ResponseMessage("welcome," + message.getName() + " !");
+    }
+
+    @SubscribeMapping("http://localhost:8081/topic/getResponse")
     public ResponseMessage say(RequestMessage message) {
         System.out.println(message.getName());
         return new ResponseMessage("welcome," + message.getName() + " !");
